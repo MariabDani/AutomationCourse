62beaeb59f5c7ff9bbd777dc83c9d41e06cf4ac4
Maria <mariabdani@gmail.com>
2025-09-17 14:29:01 +0100

    WebElements Commit

---
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -0,0 +1,28 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class WebElementCommands extends Base{
+	
+	public void verifyWebElementCommands()
+	{
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement messageTxtBox= driver.findElement(By.id("single-input-field"));
+		messageTxtBox.sendKeys("Hi Test!!");
+		WebElement showMessageBtn= driver.findElement(By.xpath("//button[@id='button-one']"));
+		showMessageBtn.click();
+		WebElement messageTxt= driver.findElement(By.xpath("//div[starts-with(text(),'Your')]"));
+		System.out.println(messageTxt.getText());
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebElementCommands webCommands= new WebElementCommands();
+		webCommands.initializeBrowser();
+		webCommands.verifyWebElementCommands();
+	}
+
+}
