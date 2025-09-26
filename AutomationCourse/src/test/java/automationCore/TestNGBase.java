3e56c58848317dc8493d3847a25abb91d567b5da
Maria <mariabdani@gmail.com>
2025-09-26 13:36:31 +0100

    TestScripts

---
+++ AutomationCourse/src/test/java/automationCore/TestNGBase.java
@@ -0,0 +1,33 @@
+package automationCore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestNGBase {
+	
+	public WebDriver driver;
+
+	@BeforeMethod
+	public void initializeBrowser()
+	{
+		driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.manage().window().maximize();
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	@AfterMethod
+	public void driverClose()
+	{
+		//driver.close();
+		//driver.quit();
+	}
+
+}
---
+++ AutomationCourse/src/test/java/testScripts/LoginTest.java
@@ -0,0 +1,57 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import automationCore.TestNGBase;
+
+public class LoginTest extends TestNGBase {
+	
+	@Test
+	public void verifyLoginWithValidCredentials()
+	{
+		WebElement userName= driver.findElement(By.id("user-name"));
+		userName.sendKeys("standard_user");
+		WebElement password= driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce");
+		WebElement loginBtn= driver.findElement(By.id("login-button"));
+		loginBtn.click();
+	}
+	
+	@Test
+	public void verifyLoginWithInvalidCredentials() {
+		
+		WebElement userName = driver.findElement(By.id("user-name"));
+		userName.sendKeys("InvalidUser");
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys("InvalidPassword");
+		WebElement loginBtn = driver.findElement(By.id("login-button"));
+		loginBtn.click();
+		
+	}
+	
+	@Test
+	public void verifyLoginValidUserNameInvalidPassword() {
+		
+		WebElement userName = driver.findElement(By.id("user-name"));
+		userName.sendKeys("standard_user");
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys("InvalidPassword");
+		WebElement loginBtn = driver.findElement(By.id("login-button"));
+		loginBtn.click();
+		
+	}
+	
+	@Test
+	public void verifyLoginInvalidUserNameValidPassword() {
+		
+		WebElement userName = driver.findElement(By.id("user-name"));
+		userName.sendKeys("InvalidUser");
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce");
+		WebElement loginBtn = driver.findElement(By.id("login-button"));
+		loginBtn.click();
+	}
+
+}
