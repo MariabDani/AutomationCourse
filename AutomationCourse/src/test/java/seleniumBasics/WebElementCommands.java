6fc50f0bee171a1d78f30346c31462ac1aa190ec
Maria <mariabdani@gmail.com>
2025-10-03 02:06:45 +0100

    Adding Wait

--- AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -1,7 +1,15 @@
 package seleniumBasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebElementCommands extends Base{
 	
@@ -11,6 +19,11 @@ public class WebElementCommands extends Base{
 		WebElement messageTxtBox= driver.findElement(By.id("single-input-field"));
 		messageTxtBox.sendKeys("Hi Test!!");
 		WebElement showMessageBtn= driver.findElement(By.xpath("//button[@id='button-one']"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(showMessageBtn));
+		
+		Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+		fluentWait.until(ExpectedConditions.elementToBeClickable(showMessageBtn));
 		showMessageBtn.click();
 		WebElement messageTxt= driver.findElement(By.xpath("//div[starts-with(text(),'Your')]"));
 		System.out.println(messageTxt.getText());
